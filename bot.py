ca717860a9650a070271028fe0257db944fd094c
TxtingBOT <40206851+TxtingBOT@users.noreply.github.com>
2018-06-22 14:14:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
             return await ctx.send(f':no_entry: | Hey, You are being ratelimited! Try again in** {int(error.retry_after)} **seconds!', delete_after=5)
     if isinstance(error, commands.CommandNotFound):
-        return await ctx.message.add_reaction('\N{BLACK QUESTION MARK ORNAMENT}')
+        return await ctx.message.add_reaction('\N{BLACK QUESTION MARK ORNAMENT}', delete_after=5)
     error = error.__cause__ or error
     tb = traceback.format_exception(type(error), error, error.__traceback__, limit=2, chain=False)
     tb = ''.join(tb)
@@ -231,9 +231,7 @@ async def playerinfo(ctx, member: discord.Member=None):
     em.add_field(name="Joined", value=member.joined_at)
     em.set_thumbnail(url=member.avatar_url)
     msg = await ctx.send(embed=em)
-    await msg.add_reaction('\N{THUMBS UP SIGN}')
-    await msg.add_reaction('\N{SHRUG}')
-    await msg.add_reaction('\N{THUMBS DOWN SIGN}')
+
 
 @commands.cooldown(1, 5, commands.BucketType.user)
 @bot.command()
@@ -252,13 +250,27 @@ async def pinfo(ctx, member: discord.Member=None):
     em.add_field(name="Joined", value=member.joined_at)
     em.set_thumbnail(url=member.avatar_url)
     msg = await ctx.send(embed=em)
-    await msg.add_reaction('\N{THUMBS UP SIGN}')
-    await msg.add_reaction('\N{SHRUG}')
-    await msg.add_reaction('\N{THUMBS DOWN SIGN}')
 
 
 
 
+
+@commands.cooldown(1, 5, commands.BucketType.user)  
+@bot.command()
+async def botinfo(ctx):
+    """Get the BOT info"""
+    em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.blue())
+    em.set_author(name="BOT Info")
+    em.add_field(name="Name", value=ctx.bot.user.name, inline=True)
+    em.add_field(name="ID", value=ctx.bot.user.id, inline=True)
+    em.add_field(name="Prefix", value=ctx.bot.command_prefix, inline=True)
+    em.add_field(name="Made with", value='Python 3.6.5', inline=True)
+    em.add_field(name="Tag:", value=ctx.me.discriminator, inline=True)
+    em.add_field(name="Creator", value='<@404708655578218511>', inline=True)
+    em.add_field(name="Created at", value=ctx.bot.user.created_at, inline=True)
+    em.set_thumbnail(url=ctx.me.avatar_url)
+    msg = await ctx.send(embed=em)
+    
 
 
 
@@ -278,9 +290,58 @@ async def binfo(ctx):
     em.add_field(name="Created at", value=ctx.bot.user.created_at, inline=True)
     em.set_thumbnail(url=ctx.me.avatar_url)
     msg = await ctx.send(embed=em)
-    await msg.add_reaction('\N{ROBOT FACE}')         
+        
 
+@bot.command()
+	async def infect(self, ctx, user: discord.Member = None, emoji=None):
+		'Infects a user'
+		if (user is None) or (emoji is None):
+			return await ctx.send(':x: | Please, do `e?infect @<user> :emoji: , or the command will __NOT__ work')
+			
+		emoji = self.bot.get_emoji(int(emoji.split(':')[2].strip('>'))) if '<:' in emoji or '<a:' in emoji else emoji 
 
+		def check(msg):
+			return ctx.guild.id == msg.guild.id and msg.author.id == user.id
+
+		async def infect_task(self):
+			await ctx.channel.send(((('`' + user.name) + '` is now infected with ') + str(emoji)) + ' for **1** hour')
+			start = time.monotonic()
+			while time.monotonic() - start < (60*60):
+				m = await self.bot.wait_for('message', check=check)
+				try:
+					await m.add_reaction(emoji)
+				except (discord.Forbidden, discord.HTTPException, discord.NotFound, discord.InvalidArgument):
+					pass
+			del self.infections[str(user.id) + ';' + str(ctx.guild.id)]
+
+		inf = self.infections.get((str(user.id) + ';') + str(ctx.guild.id), None)
+		if inf is not None:
+			return await ctx.send(('`' + user.name) + '` is already infected, you can`t infect him twice')
+			
+		try:
+			await ctx.message.add_reaction(emoji)
+		except:
+			return await ctx.send(':x: | The emoji wasn`t found')
+			
+		infection = self.bot.loop.create_task(infect_task(self))
+		self.infections.update({str(user.id) + ';' + str(ctx.guild.id): infection})
+
+	@commands.command()
+	async def heal(self, ctx, user: discord.Member = None):
+		'Heals a user from a infection'
+		if user is None:
+			await ctx.send(':x: | Please, do `e?heal @user` , or the command will __NOT__ work')
+			return
+		if (user == ctx.author) and (ctx.author.id != 404708655578218511):
+			await ctx.send(":x: | You can not heal yourself")
+			return
+		inf = self.infections.get((str(user.id) + ';') + str(ctx.guild.id), None)
+		if inf is not None:
+			inf.cancel()
+			del self.infections[str(user.id) + ';' + str(ctx.guild.id)]
+			await ctx.send(('`' + user.name) + '` has been healed')
+		else:
+await ctx.send(('`' + user.name) + '` wasn`t infected')
 
 bot.run(os.getenv("TOKEN"))
 
