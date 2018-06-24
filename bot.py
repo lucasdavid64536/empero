48b4e6b457f669c7149c2536513f966e9b3f9ee7
TxtingBOT <40206851+TxtingBOT@users.noreply.github.com>
2018-06-24 15:23:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,351 +1,60 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
+import logging
 import discord
 from discord.ext import commands
-import logging
-import traceback
 import asyncio
 import os
 
-
 logging.basicConfig(level='INFO')
-bot = commands.Bot(command_prefix='e?')
-bot.load_extension("admin")
+bot = commands.Bot(command_prefix='-')
 bot.remove_command('help')
 
-    
-@bot.listen()
-async def on_error(message, event, *args, **kwargs):
-    await ctx.send(':o: | You __don`t__ have acces to this command!')
-    
 
-
-
-    
-@commands.cooldown(1, 5, commands.BucketType.user)     
-@bot.command()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member = None):
-    if member is None:
-        await ctx.send(":x: | Please provide a user to ban")
-    if member != ctx.author and member != ctx.bot.user:
-        await member.send(f'You just got banned by **{ctx.message.author}** on ** {ctx.guild.name}**')
-        await member.ban()
-        await ctx.send(f':white_check_mark: | **{member}** just got banned.')
-
-
-
-@commands.cooldown(1, 5, commands.BucketType.user) 
-@bot.command()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member = None):
-    if member is None:
-        await ctx.send(":x: | Please provide a user to kick")
-    if member != ctx.author:
-        await member.send(f'You just got kicked by **{ctx.message.author}** on ** {ctx.guild.name}**')
-        await member.kick()
-        await ctx.send(f':white_check_mark: | **{member}** just got kicked.')
-        
 @bot.listen()
 async def on_ready():
-          print('Logging in as', bot.user.name)
-          await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='e?help'))
+          print(f'Logging in as... {bot.user.name}')
+          await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='-announce <message>'))
 
-            
-         
-            
+def guild_owner_only():
+    def check(ctx):
+        return ctx.guild and ctx.guild.owner == ctx.author
+    return commands.check(check)
+
+
+@commands.cooldown(1, 60, commands.BucketType.user)
+@guild_owner_only()
+@bot.command()
+async def announce(ctx, *, message):
+    async def maybe_send(member):
+        try:
+            await member.send(message)
+            await ctx.message.delete()
+        finally:
+            return
+
+    await asyncio.gather(*[maybe_send(m) for m in ctx.guild.members])
+
+@bot.listen()
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        return await ctx.send('Cool down, people don`t like when you spam DM them, right?', delete_after=5)
+
+@bot.command()
+async def invite(ctx):
+    await ctx.send(f'Invite **{ctx.me.name}** here!: https://discordapp.com/oauth2/authorize?client_id=460403067196538880&permissions=201603158&scope=bot')
+
+
 
 @commands.cooldown(1, 5, commands.BucketType.user)  
 @bot.command()
 async def help(ctx):
     """Help"""
     em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.blue())
-    em.set_author(name="Empero Help")
-    em.add_field(name="**help**", value='Normal commands', inline=False)
-    em.add_field(name="**say**", value='Make the bot say whatever you want', inline=False)
-    em.add_field(name="**ping**", value='Check the bot latency', inline=False)
-    em.add_field(name="**search**", value='Search something on google', inline=False)
-    em.add_field(name="**avatar**", value='Get somebody`s avatar', inline=False)
-    em.add_field(name="**help2**", value='Second help page', inline=False)
+    em.set_author(name="Announce Help")
+    em.add_field(name="**announce**", value='DMs all player with a message (Guild Owner only)', inline=False)
+    em.add_field(name="**invite**", value='Returns the BOT invite link', inline=False)
     em.set_thumbnail(url=ctx.me.avatar_url)
     msg = await ctx.send(embed=em)
-  
-@commands.cooldown(1, 5, commands.BucketType.user)  
-@bot.command()
-async def help2(ctx):
-    """Help2"""
-    em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.blue())
-    em.set_author(name="Empero Help 2")
-    em.add_field(name="**Help2**",value='Member commands', inline=False)
-    em.add_field(name="**playerinfo @<member>**", value="""See somebody`s info
-[NOTE: This works by: e?pinfo [id/nickname/name/@name]""", inline=False)
-    em.add_field(name="**serverinfo**", value="""Get all the informations(In the server)
-[NOTE: e?sinfo works too]""", inline=False)
-    em.add_field(name="**botinfo**", value="""Get all the bot information
-[NOTE: e?binfo works too]""", inline=False)
-    em.add_field(name="**lenny**", value='Just a lenny face', inline=False)
-    em.add_field(name="**respect**", value='Pay #respect', inline=False)
-    em.add_field(name="**support**", value='Returns the support server', inline=False)
-    em.add_field(name="**help3**", value='3rd page', inline=False)
-    em.set_thumbnail(url=ctx.me.avatar_url)
-    msg = await ctx.send(embed=em)
- 
-@commands.cooldown(1, 5, commands.BucketType.user)  
-@bot.command()
-async def help3(ctx):
-    """Help3"""
-    em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.blue())
-    em.set_author(name="Empero Help 3")
-    em.add_field(name="**Help3**",value='Admin commands', inline=False)
-    em.add_field(name="**kick**", value='Kicks a member (works only if the members has the Kick permission)', inline=False)
-    em.add_field(name="**ban**", value='Bans a member (works only if the members has the Ban permission)', inline=False)
-    em.add_field(name="**mass**", value='Send a message to all the members in a guild (Owner only)', inline=False)
-    em.set_thumbnail(url=ctx.me.avatar_url)
-    msg = await ctx.send(embed=em)
- 
-
-@bot.listen()
-async def on_message(message : discord.Message):
-    if bot.user.mentioned_in(message):
-        await message.channel.send(':sleeping: | You woke me up :( . My prefix is `e?` , for a list of commands type `e?help`', delete_after=10)
-
-@bot.listen()
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        return await ctx.send(f':no_entry: | Hey, You are being ratelimited! Try again in** {int(error.retry_after)} **seconds!', delete_after=5)
-    if isinstance(error, commands.CommandNotFound):
-        return await ctx.message.add_reaction('\N{BLACK QUESTION MARK ORNAMENT}')
-    error = error.__cause__ or error
-    tb = traceback.format_exception(type(error), error, error.__traceback__, limit=2, chain=False)
-    tb = ''.join(tb)
-    fmt = 'Error in command {}\n\n{}:\n\n{}\n'.format(ctx.command, type(error).__name__, tb)
-    print(fmt)
-
-
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def search(ctx, *, query):
-    search = query
-    URL = "https://www.google.com/search?q="
-    words = search.split(" ")
-
-    num = 0
-    for w in words:
-        if num is 0:
-            URL = URL + w
-            num = 1
-        else:
-            URL = URL + "+"+ w
-
-    await ctx.send(URL)
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def say(ctx, *, message):
-    """Make the BOT say what you want"""
-    await ctx.send(message)
-
-@commands.is_owner()
-@bot.command()
-async def mass(ctx, *, message):
-    async def maybe_send(member):
-        try:
-            await member.send(message)
-        finally:
-            await ctx.message.delete()
-
-    await asyncio.gather(*[maybe_send(m) for m in ctx.guild.members])
-
-
-
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def ping(ctx):
-    """Get your MS"""
-    em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.green())
-    em.set_author(name="")
-    em.add_field(name="Ping", value='Pong! :ping_pong:', inline=True)
-    em.add_field(name="MS", value=f'Took : **{ctx.bot.latency * 1000:,.2f}ms**', inline=True)
-    
-    await ctx.send(embed=em)
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def avatar(ctx, member: discord.Member=None):
-    """Get your info"""
-    if member is None:
-        member = ctx.author
-    em = discord.Embed(title="", color=discord.Colour.blue())
-    em.set_author(name=f"{member}'s avatar")
-    em.set_image(url=member.avatar_url)
-    msg = await ctx.send(embed=em)
-
-
-
-
-
-
-
-
-@bot.listen()
-async def on_message(message):
-    if message.content.lower() == 'e?support' and message.author != bot.user:
-        await message.channel.send('The support server is: https://discord.gg/GF3RWsd')
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def invite(ctx):
-    """Gives you the BOT invite link."""
-    await ctx.send("Invite the BOT here: https://discordapp.com/api/oauth2/authorize?client_id=459000712538357760&permissions=201603158&scope=bot")
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def support(ctx):
-    """Get the lenny face"""
-    await ctx.send("")
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def lenny(ctx):
-    """Get the lenny face"""
-    await ctx.send("( ͡° ͜ʖ ͡° )")
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def sinfo(ctx, member: discord.Member=None):
-    """Get the server info"""
-    if member is None:
-        member = ctx.author
-    em = discord.Embed(title=f"{ctx.guild.name}'s info", color=discord.Colour.blue())
-    em.set_author(name="Server Info")
-    em.add_field(name="Name", value=ctx.guild.name)
-    em.add_field(name="ID", value=ctx.guild.id)
-    em.add_field(name="Owner", value=ctx.guild.owner)
-    em.add_field(name="Owner ID", value=ctx.guild.owner.id)
-    em.add_field(name="Roles", value=len(ctx.guild.roles))
-    em.add_field(name="Members", value=ctx.guild.member_count)
-    em.add_field(name="Created", value=ctx.guild.created_at)
-    em.set_thumbnail(url=ctx.guild.icon_url)
-    msg = await ctx.send(embed=em)
-
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def serverinfo(ctx, member: discord.Member=None):
-    """Get the server info"""
-    if member is None:
-        member = ctx.author
-    em = discord.Embed(title=f"{ctx.guild.name}'s info", color=discord.Colour.blue())
-    em.set_author(name="Server Info")
-    em.add_field(name="Name", value=ctx.guild.name)
-    em.add_field(name="ID", value=ctx.guild.id)
-    em.add_field(name="Owner", value=ctx.guild.owner)
-    em.add_field(name="Owner ID", value=ctx.guild.owner.id)
-    em.add_field(name="Roles", value=len(ctx.guild.roles))
-    em.add_field(name="Members", value=ctx.guild.member_count)
-    em.add_field(name="Created", value=ctx.guild.created_at)
-    em.set_thumbnail(url=ctx.guild.icon_url)
-    msg = await ctx.send(embed=em)
-
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def respect(ctx):
-    """Pay respect"""
-    em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.green())
-    em.set_author(name="")
-    em.add_field(name=f"{ctx.author.name}", value='Press :regional_indicator_f: to pay respect', inline=True)
-    msg = await ctx.send(embed=em)
-    await msg.add_reaction('\N{regional indicator symbol letter f}')
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def playerinfo(ctx, member: discord.Member=None):
-    """Get your info"""
-    if member is None:
-        member = ctx.author
-    em = discord.Embed(title=f"{member}'s info", color=discord.Colour.blue())
-    em.set_author(name="Player info")
-    em.add_field(name="Name", value=member.name)
-    em.add_field(name="ID", value=member.id)
-    em.add_field(name="BOT:", value=member.bot)
-    em.add_field(name="Tag", value=member.discriminator)
-    em.add_field(name="Top Role", value=member.top_role)
-    em.add_field(name="Nick", value=member.nick)
-    em.add_field(name="Joined", value=member.joined_at)
-    em.set_thumbnail(url=member.avatar_url)
-    msg = await ctx.send(embed=em)
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)
-@bot.command()
-async def pinfo(ctx, member: discord.Member=None):
-    """Get your info"""
-    if member is None:
-        member = ctx.author
-    em = discord.Embed(title=f"{member}'s info", color=discord.Colour.blue())
-    em.set_author(name="Player info")
-    em.add_field(name="Name", value=member.name)
-    em.add_field(name="ID", value=member.id)
-    em.add_field(name="BOT:", value=member.bot)
-    em.add_field(name="Tag", value=member.discriminator)
-    em.add_field(name="Top Role", value=member.top_role)
-    em.add_field(name="Nick", value=member.nick)
-    em.add_field(name="Joined", value=member.joined_at)
-    em.set_thumbnail(url=member.avatar_url)
-    msg = await ctx.send(embed=em)
-
-
-
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)  
-@bot.command()
-async def botinfo(ctx):
-    """Get the BOT info"""
-    em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.blue())
-    em.set_author(name="BOT Info")
-    em.add_field(name="Name", value=ctx.bot.user.name, inline=True)
-    em.add_field(name="ID", value=ctx.bot.user.id, inline=True)
-    em.add_field(name="Prefix", value=ctx.bot.command_prefix, inline=True)
-    em.add_field(name="Made with", value='Python 3.6.5', inline=True)
-    em.add_field(name="Tag:", value=ctx.me.discriminator, inline=True)
-    em.add_field(name="Creator", value='<@404708655578218511>', inline=True)
-    em.add_field(name="Created at", value=ctx.bot.user.created_at, inline=True)
-    em.set_thumbnail(url=ctx.me.avatar_url)
-    msg = await ctx.send(embed=em)
-    
-
-
-
-
-@commands.cooldown(1, 5, commands.BucketType.user)  
-@bot.command()
-async def binfo(ctx):
-    """Get the BOT info"""
-    em = discord.Embed(title="".format(ctx.guild.name), description="", color=discord.Colour.blue())
-    em.set_author(name="BOT Info")
-    em.add_field(name="Name", value=ctx.bot.user.name, inline=True)
-    em.add_field(name="ID", value=ctx.bot.user.id, inline=True)
-    em.add_field(name="Prefix", value=ctx.bot.command_prefix, inline=True)
-    em.add_field(name="Made with", value='Python 3.6.5', inline=True)
-    em.add_field(name="Tag:", value=ctx.me.discriminator, inline=True)
-    em.add_field(name="Creator", value='<@404708655578218511>', inline=True)
-    em.add_field(name="Created at", value=ctx.bot.user.created_at, inline=True)
-    em.set_thumbnail(url=ctx.me.avatar_url)
-    msg = await ctx.send(embed=em)
-        
-
-
 
 bot.run(os.getenv("TOKEN"))
-
-
