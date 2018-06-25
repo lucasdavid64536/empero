46ee9fdcf4c4fa022fab20c6aaa6362a8e499349
TxtingBOT <40206851+TxtingBOT@users.noreply.github.com>
2018-06-25 21:04:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,31 @@ import logging
 import traceback
 import asyncio
 import os
+from discord import opus
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+
+    if opus.is_loaded():
+
+        return True
+
+    for opus_lib in opus_libs:
+
+        try:
+
+            opus.load_opus(opus_lib)
+
+            return
+
+        except OSError:
+
+            pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
+
+load_opus_lib()
 
 
 
